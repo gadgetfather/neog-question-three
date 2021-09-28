80ea8a280586f06d779582afc573e99ce3eadbe1
Akshay Kamble <kamblea635@gmail.com>
2021-09-28 14:41:47 +0530

    completed

--- app.js
+++ app.js
@@ -5,5 +5,21 @@ var btnThree = document.querySelector("#button3")
 var outputEl = document.querySelector("#output")
 
 btnOne.addEventListener('click',function clickHandler1(){
-    
+    var inputData = input.value
+    outputEl.innerHTML = `<h1>${inputData}</h1>`
+
+})
+
+
+btnTwo.addEventListener('click',function clickHandler1(){
+    var inputData = input.value
+    outputEl.innerHTML = `<h2>${inputData}</h2>`
+
+})
+
+
+
+btnThree.addEventListener('click',function clickHandler1(){
+    var inputData = input.value
+    outputEl.innerHTML = `<h3>${inputData}</h3>`
 })
