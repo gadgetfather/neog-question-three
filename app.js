0a2d9c26f167807c95c18e04199d95769d296a89
Akshay Kamble <kamblea635@gmail.com>
2021-09-28 14:32:24 +0530

    linked buttons

---
+++ app.js
@@ -0,0 +1,9 @@
+var input = document.querySelector(".input")
+var btnOne = document.querySelector("#button1")
+var btnTwo = document.querySelector("#button2")
+var btnThree = document.querySelector("#button3")
+var outputEl = document.querySelector("#output")
+
+btnOne.addEventListener('click',function clickHandler1(){
+    
+})
